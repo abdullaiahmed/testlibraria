ecb2f8a2153605e2fa82062ae77e236f9d420a93
Ahmed Abdullai <ahmed.abdullai@drivosity.com>
2022-05-04 00:05:35 +0200

    first commit

---
+++ src/main/java/com/ahmedabdullai/Calculator.java
@@ -0,0 +1,13 @@
+package com.ahmedabdullai;
+
+public class Calculator {
+    /**
+     *
+     * @param a first input
+     * @param b second input
+     * @return sum of the input numbers
+     */
+    int add(int a, int b){
+        return a+b;
+    }
+}
